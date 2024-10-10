ab7ae15975ad895a506b86095f45ce856fc1e9e1
juanq4 <juan.mejia@q4inc.com>
2024-10-10 17:32:42 -0400

    wip

---
+++ TODO.md
@@ -0,0 +1,5 @@
+// fix readme file
+// Add eslint
+// Add prettier
+// use async logger instead of console.log
+// export as npm package
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,28 @@
 import "dotenv/config";
 import * as fs from "fs";
 import { TestCase } from "./definitions/test-case.definition";
-// import transformToGherkin from "./utils/transform-to-gherkin.util";
 import upsertTestCase from "./controllers/upsert-test-case.controller";
 import extractTestCases from "./utils/extract-test-cases.util";
 import getTestFilesPaths from "./utils/get-test-files-paths.util";
 import logError from "./utils/log-error.util";
 import toTestCase from "./utils/to-test-case.util";
+import transformToGherkin from "./utils/transform-to-gherkin.util";
 
 const dirPath = process.env.DIR_PATH;
+const isOpenAIEnabled = process.env.OPENAI_IS_ENABLED === "true";
 
 const updateTestCases = async (fileContent: string, testCases: TestCase[]): Promise<string | undefined> => {
   let updatedContent;
 
-  // You might be tempted to use forEach prototype, but forEach doesn't wait for asynchronous operations
-  // inside its callback to complete before moving on to the next iteration or finishing execution. Even
-  // if you're using await inside the loop, forEach itself doesn't pause. This means the loop finishes
-  // iterating and executes before any of the await calls within the loop have resolved.
+  // You might be tempted to use an array prototype (e.g forEach), but they don't wait for asynchronous
+  // operations inside their callback to complete before moving on to the next iteration or finishing execution.
+  // Even if you're using await inside the loop, the prototype itself doesn't pause. This means the
+  // loop finishes iterating and executes before any of the await calls within the loop have resolved.
   for (const testCase of testCases) {
     console.log("Updating test case", testCase);
 
     try {
-      // FIXME: add conditional for OpenAI
-      // const gherkinTitle = await transformToGherkin(test.description);
-      const gherkinTitle = testCase.title;
+      const gherkinTitle = isOpenAIEnabled ? await transformToGherkin(testCase.title) : testCase.title;
       if (!gherkinTitle) {
         console.warn("Unable to convert current test to Gherkin", testCase.title);
         return;
@@ -47,10 +46,10 @@ const updateTestCases = async (fileContent: string, testCases: TestCase[]): Prom
 const updateTestFiles = async () => {
   const filesPaths = await getTestFilesPaths(dirPath);
 
-  // You might be tempted to use forEach prototype, but forEach doesn't wait for asynchronous operations
-  // inside its callback to complete before moving on to the next iteration or finishing execution. Even
-  // if you're using await inside the loop, forEach itself doesn't pause. This means the loop finishes
-  // iterating and executes before any of the await calls within the loop have resolved.
+  // You might be tempted to use an array prototype (e.g forEach), but they don't wait for asynchronous
+  // operations inside their callback to complete before moving on to the next iteration or finishing execution.
+  // Even if you're using await inside the loop, the prototype itself doesn't pause. This means the
+  // loop finishes iterating and executes before any of the await calls within the loop have resolved.
   for (const filePath of filesPaths) {
     console.log("Updating test file", filePath);
 
